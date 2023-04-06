3e12c8ba62fc7e9b20568f2ff17258e7df0c28dd
lachtak <lazynewocean@gmail.com>
2023-04-06 22:04:39 +0300

    main upd

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 25491;
+        int bonus = 20;
+
+        System.out.println("Спасибо за покупку билета и приятного полёта!");
+        System.out.println("Вам начислено " + (price / bonus) + " бонусных миль.");
+    }
+}
